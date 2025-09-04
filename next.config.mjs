aec1862e7b95f1616f858f9c85055f7e650ffbca
MiguelSilva <43246456+MiguelADSilva@users.noreply.github.com>
2025-09-04 23:49:17 +0100

    refactor

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
   output: 'standalone', // Otimiza para deploy
   images: {
     unoptimized: true // Para deploy estático
