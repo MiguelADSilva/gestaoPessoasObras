02b6a79172c089dc57b90f529c89f054e0da8d64
MiguelSilva <43246456+MiguelADSilva@users.noreply.github.com>
2025-09-04 23:58:50 +0100

    refactor

--- next.config.js
+++ next.config.js
@@ -1,11 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'standalone',
-  compress: true,
-  images: {
-    unoptimized: true // Importante para Netlify
+  experimental: {
+    appDir: true,
   },
-  // Remova qualquer configuração experimental problemática
 }
 
-module.exports = nextConfig
+export default nextConfig;
